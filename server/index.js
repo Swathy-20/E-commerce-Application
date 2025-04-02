5148975ece4ba1175f28127f75495f60c638efbf
Swathy-20 <swathyks202001@gmail.com>
2025-04-02 21:24:58 +0530

    cors updated

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ connectDB()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-        origin: ["http://localhost:5173","https://e-commerce-application-server-sigma.vercel.app"],
+        origin: ["http://localhost:5173","https://e-commerce-application-shopnest.vercel.app"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTION"],
     })
